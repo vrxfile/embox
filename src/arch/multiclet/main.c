0e88bb885efef853c4066b751dd47bbf50a9a147
Anton Bondarev <anton.bondarev2310@gmail.com>
2015-07-30 16:16:45 +0300

    arch: Add crt0.S from source

--- src/arch/multiclet/main.c
+++ src/arch/multiclet/main.c
@@ -1,8 +1,8 @@
 /**
- * @file 
+ * @file
  * @brief
  *
- * @author  Anton Kozlov 
+ * @author  Anton Kozlov
  * @date    21.07.2015
  */
 
@@ -10,13 +10,9 @@
 
 extern void kernel_start(void);
 
-struct diag;
-extern void mcp_putc(const struct diag *dev, char ch);
-extern void mcp_init(const struct diag *dev);
+int errno;
 
 int main(void) {
-	mcp_init(NULL);
-	mcp_putc(NULL, 'a');
-	/*kernel_start();*/
+	kernel_start();
+	return 0;
 }
-
